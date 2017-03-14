4dbc5b0c1f87296117f100277e1dd6ca5502df10
Antti <anttis.pitkanen@gmail.com>
2017-03-14 19:46:01 +0200

    first working script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 const readline = require('readline');
+const async = require('async');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -8,36 +9,240 @@ const rl = readline.createInterface({
     prompt: '> '
 })
 
-/*
+
 console.log('Pelataan Wikipedia-peliä!');
 console.log('Syötä mikä vaan suomenkielinen wikipedia-artikkeli (koko url):');
-*/
 
-/*
+const wikiURL = 'https://fi.wikipedia.org';
+
+const targetLink = 'https://fi.wikipedia.org/wiki/Filosofia';
+
+let startURL = null;
+let results = [];
+let count = 0;
+//let startURL = testURL7;
+let currentURL;
+let linksInSingleArticle = [];
+
+
 rl.prompt();
 
 rl.on('line', (line) => {
     console.log('lol kirjoitit että ' + line + ' :D');
-    rl.close();
+
+    startURL = line.trim();
+    currentURL = startURL;
+
+    async.whilst(
+
+        () => { return currentURL !== null && currentURL !== targetLink && results.indexOf(currentURL) === -1 },
+
+        (callback) => {
+
+            axios.get(currentURL).then(response => {
+                let $ = cheerio.load(response.data);
+                console.log($('.firstHeading').text());
+
+
+
+                let pText = $('#mw-content-text > p').text();
+
+                let indexOfFirstOpeningParenthesis = pText.indexOf('('); //find the indexOf first '('
+                let indexOfFirstClosingParenthesis = pText.indexOf(')'); //find the indexOf first ')'
+
+                $('#mw-content-text > p a').each((i, elem) => {
+                    let link = $(elem);
+
+
+                    if(!link.text().match(/^\[/) && //filter out all remarks like "[1]"
+                        link.attr('href').startsWith('/wiki/') && //check that links are valid articles and not e.g. scripts
+                        !link.text().match(/^\d+$/) && //filter out years
+                        !link.attr('href').match(/^\/wiki\/\d+\..+kuuta/) &&
+                        !link.attr('href').match(/^\/wiki\/Tiedosto:/)) { //filter out dates
+
+                        let i = pText.indexOf(link.text());
+
+                        if (!(i > indexOfFirstOpeningParenthesis && i < indexOfFirstClosingParenthesis)) { //check that the link is not between parentehses
+                            linksInSingleArticle.push(link.attr('href'));
+                        }
+
+                    }
+                })
+
+                console.log();
+
+                //console.log(linksInSingleArticle);
+                //console.log();
+
+                results.push(currentURL);
+
+                currentURL = wikiURL + linksInSingleArticle[0];
+                console.log(currentURL);
+
+
+                count++;
+                console.log(count);
+
+                linksInSingleArticle = [];
+
+                callback(null, count);
+            })
+            .catch(err => {
+                console.log('*********** ERROR ***********');
+                console.log(err);
+            })
+
+        },
+
+        (err, n) => {
+            if (results.indexOf(currentURL) === -1) {
+                console.log(`it took ${count} steps to get from ${startURL} to Filosofia :D`);
+            } else {
+                console.log(`woops! ${currentURL} started a loop!`);
+            }
+
+            process.exit(0);
+        }
+    )
+
 }).on('close', () => {
     process.exit(0);
 })
+
+
+
+
+// let testURL1 = 'https://fi.wikipedia.org/wiki/Tieto';
+// let testURL2 = 'https://fi.wikipedia.org/wiki/Andr%C3%A9_Previn';
+// let testURL3 = 'https://fi.wikipedia.org/wiki/Oikeutusteoria';
+// let testURL4 = 'https://fi.wikipedia.org/wiki/Propositio';
+// let testURL5 = 'https://fi.wikipedia.org/wiki/J%C3%A4knabackenin_j%C3%A4tinkirkko';
+// let testURL6 = 'https://fi.wikipedia.org/wiki/Espanjan_vastarintaliike';
+// let testURL7 = 'https://fi.wikipedia.org/wiki/Ealing_Studios';
+// let testURL8 = 'https://fi.wikipedia.org/wiki/Adolf_Hitler';
+// let testURL9 = 'https://fi.wikipedia.org/wiki/Enemmist%C3%B6_(%C3%A4%C3%A4nestys)';
+// let testURL10 = 'https://fi.wikipedia.org/wiki/Julkisoikeus';
+// let testURL11 = 'https://fi.wikipedia.org/wiki/Anna_Abreu';
+
+
+//let links = [testURL1];
+//let numOfPagesVisited = 0;
+
+
+/*
+async.whilst(
+
+    () => { return currentURL !== null && currentURL !== targetLink && results.indexOf(currentURL) === -1 },
+
+    (callback) => {
+
+        axios.get(currentURL).then(response => {
+            let $ = cheerio.load(response.data);
+            console.log($('.firstHeading').text());
+
+
+
+            let pText = $('#mw-content-text > p').text();
+
+            let indexOfFirstOpeningParenthesis = pText.indexOf('('); //find the indexOf first '('
+            let indexOfFirstClosingParenthesis = pText.indexOf(')'); //find the indexOf first ')'
+
+            $('#mw-content-text > p a').each((i, elem) => {
+                let link = $(elem);
+
+
+                if(!link.text().match(/^\[/) && //filter out all remarks like "[1]"
+                    link.attr('href').startsWith('/wiki/') && //check that links are valid articles and not e.g. scripts
+                    !link.text().match(/^\d+$/) && //filter out years
+                    !link.attr('href').match(/^\/wiki\/\d+\..+kuuta/) &&
+                    !link.attr('href').match(/^\/wiki\/Tiedosto:/)) { //filter out dates
+
+                    let i = pText.indexOf(link.text());
+
+                    if (!(i > indexOfFirstOpeningParenthesis && i < indexOfFirstClosingParenthesis)) { //check that the link is not between parentehses
+                        linksInSingleArticle.push(link.attr('href'));
+                    }
+
+                }
+            })
+
+            console.log();
+
+            //console.log(linksInSingleArticle);
+            //console.log();
+
+            results.push(currentURL);
+
+            currentURL = wikiURL + linksInSingleArticle[0];
+            console.log(currentURL);
+
+
+            count++;
+            console.log(count);
+
+            linksInSingleArticle = [];
+
+            callback(null, count);
+        })
+        .catch(err => {
+            console.log('*********** ERROR ***********');
+            console.log(err);
+        })
+
+    },
+
+    (err, n) => {
+        if (results.indexOf(currentURL) === -1) {
+            console.log(`it took ${count} steps to get from ${startURL} to ${currentURL}`);
+        } else {
+            console.log(`woops! ${currentURL} started a loop!`);
+        }
+
+        process.exit(0);
+    }
+)
 */
 
-
-let testURL = 'https://fi.wikipedia.org/wiki/Tieto';
-
-axios.get(testURL)
+/*
+axios.get(nextLink)
 .then(response => {
-    console.log(response.data);
-    console.log();
+    numOfPagesVisited++;
+    console.log(`visited ${numOfPagesVisited} pages`);
+
     let $ = cheerio.load(response.data);
     console.log($('.firstHeading').text());
-    console.log($('#mw-content-text').text());
+    //console.log($('#mw-content-text p').text());
+    $('#mw-content-text p a').each((i, elem) => {
+        let link = $(elem);
+
+        //filter out all remarks like "[1]"
+        if(!link.text().match(/^\[/)) {
+            links.push(link);
+        }
+    })
+
+    let fullURL = wikiURL + $(links[0]).attr('href');
+    console.log(fullURL);
+    if (fullURL === targetLink) {
+        stillSearching = false;
+        console.log(`From ${testURL} to Filosofia in ${numOfPagesVisited} steps!`);
+
+    }*/  /* else {
+        nextLink =
+    }*/
+
+    /*links.map(link => {
+        console.log($(link).text() + ': ' + wikiURL + $(link).attr('href'));
+    })*/
+
+/*
 })
 .catch(err => {
     console.error(error);
+    stillSearching = false;
 })
+*/
+
 
 
 
