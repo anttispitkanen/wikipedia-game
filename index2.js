e6e2566582479333e8da2786ecf7812b51c86c04
Antti <anttis.pitkanen@gmail.com>
2017-03-20 23:14:57 +0200

    new working file in index2.js

--- index2.js
+++ index2.js
@@ -17,12 +17,8 @@ const wikiURL = 'https://fi.wikipedia.org';
 const targetLink = 'https://fi.wikipedia.org/wiki/Filosofia';
 
 let startURL = null;
-let results = [];
-let count = 0;
 
 let currentURL;
-let linksInSingleArticle = [];
-
 
 console.log('Pelataan Wikipedia-peliä!');
 console.log('Syötä mikä vaan suomenkielinen wikipedia-artikkeli (koko url tai hakusana):');
@@ -37,27 +33,45 @@ rl.on('line', (line) => {
     line = line.trim();
 
     if (line.startsWith('http://') || line.startsWith('https://')) { //test if line is a query string or a link
-        if (line.match(/fi.wikipedia.org\/wiki\//) &&
-            !line.match(/fi.wikipedia.org\/wiki\/Toiminnot:/) &&
-            !line.match(/fi.wikipedia.org\/wiki\/Tiedosto:/)) { //test if link is valid
+
+        //THIS WAY WITH A FULL URL GIVEN
+        if (validateURL(line)) { //test if link is valid
 
             startURL = line.trim();
+            playGame(startURL);
+
         } else {
             console.log('Ei tuollainen haku kelpaa :D');
             process.exit(0);
         }
     } else {
+        //THIS WAY WITH A SEARCH TERM
+
         //search the wikipedia API with the given search term
         //startURL = fetchWithQueryString(line);
         fetchWithQueryString(line).then(fulfilled => {
             startURL = fulfilled;
+            console.log(startURL);
+            console.log('täällä oltaisiin promisen thenissä :D')
+            playGame(startURL);
         })
     }
 
-    console.log(startURL);
 
-    //startURL = line.trim();
-    currentURL = startURL;
+
+}).on('close', () => {
+    process.exit(0);
+})
+
+
+//the actual crawling through links
+function playGame(startURL) {
+
+    let currentURL = startURL;
+
+    let results = [];
+    let count = 0;
+    let linksInSingleArticle = [];
 
     async.whilst(
 
@@ -69,8 +83,6 @@ rl.on('line', (line) => {
                 let $ = cheerio.load(response.data);
                 console.log($('.firstHeading').text());
 
-
-
                 let pText = $('#mw-content-text > p').text();
 
                 let indexOfFirstOpeningParenthesis = pText.indexOf('('); //find the indexOf first '('
@@ -79,20 +91,13 @@ rl.on('line', (line) => {
                 $('#mw-content-text > p a').each((i, elem) => {
                     let link = $(elem);
 
-
-                    if(!link.text().match(/^\[/) && //filter out all remarks like "[1]"
-                        link.attr('href').startsWith('/wiki/') && //check that links are valid articles and not e.g. scripts
-                        !link.text().match(/^\d+$/) && //filter out years
-                        !link.attr('href').match(/^\/wiki\/\d+\..+kuuta/) && //filter out dates
-                        !link.attr('href').match(/^\/wiki\/Tiedosto:/) && //filter out files
-                        !link.attr('href').match(/^\/wiki\/Toiminnot:/)) { //filter out actions
+                    if(filterOutWrongLinks(link)) {
 
                         let i = pText.indexOf(link.text());
 
                         if (!(i > indexOfFirstOpeningParenthesis && i < indexOfFirstClosingParenthesis)) { //check that the link is not between parentehses
                             linksInSingleArticle.push(link.attr('href'));
                         }
-
                     }
                 })
 
@@ -101,8 +106,17 @@ rl.on('line', (line) => {
 
                 results.push(currentURL);
 
+                //FIXME: situation where there's no links in the p text
+
+                if (!linksInSingleArticle[0]) {
+                    //callback('error!!!!')
+                    console.log('\nHups!');
+                    console.log(`${$('.firstHeading').text()} ei johtanut mihinkään\n`);
+                    console.log(`¯\\_(ツ)_/¯\n\n`);
+                    process.exit(0);
+                }
+
                 currentURL = wikiURL + linksInSingleArticle[0];
-                if (!currentURL) { callback('error!!!!') }
 
                 console.log(currentURL);
 
@@ -126,16 +140,14 @@ rl.on('line', (line) => {
             if (results.indexOf(currentURL) === -1) {
                 console.log(`it took ${count} steps to get from ${startURL} to Filosofia :D`);
             } else {
-                console.log(`woops! ${currentURL} started a loop!`);
+                console.log(`\nHups!\n\n ${currentURL} aloitti luupin\n\n ¯\\_(ツ)_/¯!`);
             }
 
             process.exit(0);
         }
     )
+}
 
-}).on('close', () => {
-    process.exit(0);
-})
 
 
 
@@ -149,9 +161,9 @@ function fetchWithQueryString(query) {
     //return Promise.resolve(
         return axios.get(queryURL).then(response => {
             console.log('axiosin sisällä :D');
-            let data = response[0];
-            console.log(data);
-            return data;
+            let newLink = response.data[3][0];
+            console.log(newLink);
+            return newLink;
 
         })
         .catch(err => {
@@ -166,4 +178,35 @@ function fetchWithQueryString(query) {
 
     //let $ = cheerio.load(response.data);
     //console.log($('.firstHeading').text());
+}
+
+
+//takes a link (cheerio element) as input and returns true if it's valid, false otherwise
+function filterOutWrongLinks(link) {
+
+    const linkText = link.text();
+    const linkHref = link.attr('href');
+
+    if(!linkText.match(/^\[/) && //filter out all remarks like "[1]"
+        linkHref.startsWith('/wiki/') && //check that links are valid articles and not e.g. scripts
+        !linkText.match(/^\d+$/) && //filter out years
+        !linkHref.match(/^\/wiki\/\d+\..+kuuta/) && //filter out dates
+        !linkHref.match(/^\/wiki\/Tiedosto:/) && //filter out files
+        !linkHref.match(/^\/wiki\/Toiminnot:/)) { //filter out actions
+
+        return true;
+    }
+    return false;
+}
+
+
+//takes a search string and returns true if it's valid, false otherwise
+function validateURL(url) {
+    if (url.match(/fi.wikipedia.org\/wiki\//) &&
+        !url.match(/fi.wikipedia.org\/wiki\/Toiminnot:/) &&
+        !url.match(/fi.wikipedia.org\/wiki\/Tiedosto:/)) { //test if link is valid)
+
+        return true;
+    }
+    return false;
 }
